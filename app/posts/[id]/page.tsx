5f7b316cc8e80c7050b8e1b1a36669e839e9f88e
seon1021 <hseon@hs.ac.kr>
2026-04-30 11:57:38 +0900

    fix: 수정된 파라미터 구조로 게시글 ID 추출 방식 개선

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -6,7 +6,7 @@ import PostSidebar from '../../components/PostSidebar'
 import CommentSection from '../../components/CommentSection'
 
 export default async function Page({ params }: { params: any }) {
-  const { id } = await params
+  const { id } = params
   const post = await getPost(Number(id))
 
   if (!post) {
